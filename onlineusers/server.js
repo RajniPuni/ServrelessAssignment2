d2701ea3d0000eab78e91a44587c62987c0d5ea0
RajniPuni <78663429+RajniPuni@users.noreply.github.com>
2021-06-12 10:25:57 -0300

    Update server.js

--- onlineusers/server.js
+++ onlineusers/server.js
@@ -22,13 +22,29 @@ app.get('/', function (req, res) {
             onlineu = result[0].username;
             res.write('<html>');
             res.write('<head> <title>Online users:</title> </head>');
-            res.write(' <body> <div>Online users:</div><div>'+ onlineu+'</div></body>');
+            res.write(' <body> <div>Online users:</div><div>'+ onlineu+'</div><br/></body><form method="post" action="/"><input type="hidden" name="name"><input type="submit" value="Logout"></form>');
             res.write('</html>');
         }
     });
     
     //res.sendFile(__dirname + '/onlineusers.html');
 });
+
+app.post('/', function(request, response){
+    username = request.body.name;
+    con.connect(function(err) {
+            
+        console.log("Connected!");
+        var sqlupdate = "UPDATE userstate SET logouttime=sysdate() where username='"+username+"'";
+        con.query(sqlupdate, function (err, result) {
+            if (err) throw err;
+            console.log("1 record updated");
+        });
+        return response.send({ "message": "user logged out" });
+    });
+    
+});
+
 
 var server = app.listen(4002, function () {
     console.log('Node server is running..');
